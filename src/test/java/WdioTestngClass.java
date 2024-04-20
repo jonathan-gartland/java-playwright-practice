76625ad40e465cbb12fea82586f541b5d789c2a1
jonathan-gartland <jonathan-gartland@gmail.com>
2024-04-20 10:27:01 -0400

    added log4j to deps, still a bit wonky saying its unresolved, but it fixes the warning message, so keep like this for now and try to fix.

---
+++ src/test/java/WdioTestngClass.java
@@ -0,0 +1,17 @@
+import com.microsoft.playwright.*;
+import org.testng.annotations.Test;
+
+import java.nio.file.Paths;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static java.util.Arrays.asList;
+
+public class WdioTestngClass {
+    @Test
+    public void testCase1() {
+        System.out.println("in test case 1 of WdioTestngClass");
+        String name = "joeJumpinJolly";
+        assert name.equals("joeJumpinJolly") == true;
+        assert 1 == 1;
+    }
+}
