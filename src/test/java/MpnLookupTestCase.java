be22b4d62b2d1538cac6043ce51e513166910d39
jonathan-gartland <jonathan-gartland@gmail.com>
2024-04-21 17:22:58 -0400

    recreated simple playwright tests for mpn lookup in java with testng, original is a javascript project using jest & playwright

---
+++ src/test/java/MpnLookupTestCase.java
@@ -0,0 +1,98 @@
+import com.microsoft.playwright.*;
+import org.testng.annotations.*;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+public class MpnLookupTestCase {
+    // Shared between all tests in this class.
+    Playwright playwright;
+    Browser browser;
+
+    // New instance for each test method.
+    BrowserContext context;
+    Page page;
+
+    @BeforeClass
+    void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+    }
+
+    @AfterClass
+    void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeMethod
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+        page.navigate("localhost:3000/practice-pages");
+    }
+
+    @AfterMethod
+    void closeContext() {
+        context.close();
+    }
+
+    @Test
+    public void MpnLookupPageTitle() {
+        Locator titleLabel = page.getByTestId("mpn-lookup-title");
+        assertThat(titleLabel).containsText("MPN Lookup");
+    }
+
+    @Test
+    public void AboutSection() {
+        Locator qtInfoLink = page.getByTestId("qtlink");
+        qtInfoLink.click();
+        page.waitForURL("**/quanti-tray-system/", new Page.WaitForURLOptions().setTimeout(5000));
+        Locator qtInfoHeader = page.locator("#page-top > div.page-content > div > div:nth-child(1) > div:nth-child(2) > h1");
+        assertThat(qtInfoHeader).isVisible();
+        page.goBack(new Page.GoBackOptions().setTimeout(5000));
+        assertThat(qtInfoLink).isVisible();
+    }
+
+    @Test
+    public void MpnGeneratorPageLinks() {
+        Locator mpnGeneratorLink = page.getByTestId("mpngen");
+        mpnGeneratorLink.click();
+        page.waitForURL("**/mpn-generator/", new Page.WaitForURLOptions().setTimeout(5000));
+        Locator mpnGeneratorHeader = page.locator("#page-top > div.page-content > div > div:nth-child(1) > div:nth-child(2) > h1");
+        assertThat(mpnGeneratorHeader).isVisible();
+        page.goBack(new Page.GoBackOptions().setTimeout(5000));
+        assertTrue(mpnGeneratorLink.isVisible());
+    }
+
+    @Test
+    public void TestMpnSpa() {
+        Locator inputWellsPos = page.locator("#qtinput");
+        assertTrue(inputWellsPos.isVisible());
+        int posWellCount = Integer.parseInt(inputWellsPos.inputValue());
+        assertEquals(posWellCount, 0);
+        String qtMpnVal = page.getByTestId("qt-mpn-val").innerText();
+        Dictionary<Integer, Double> mpnValues = new Hashtable<Integer, Double>();
+        mpnValues.put(51, 146.1);
+        mpnValues.put(1, 0.3);
+        mpnValues.put(18, 14.1);
+        mpnValues.put(33, 37.5);
+
+        Enumeration<Integer> keys = mpnValues.keys();
+        while (keys.hasMoreElements()) {
+            Integer key = keys.nextElement();
+            inputWellsPos.fill(Integer.toString(key));
+            Double newMpnVal = Double.valueOf(page.getByTestId("qt-mpn-val").innerText().split(":")[1]);
+            assertEquals(newMpnVal, mpnValues.get(key));
+        }
+        inputWellsPos.fill("");
+        String emptyMessage = page.getByTestId("empty-value-label").innerText();
+        assertEquals(emptyMessage, "Enter valid value please!");
+
+
+    }
+}
--- src/test/java/NewTestCase.java
+++ src/test/java/NewTestCase.java
@@ -1,5 +1,5 @@
 import com.microsoft.playwright.*;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.nio.file.Paths;
 import java.util.List;
@@ -9,26 +9,47 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 
 public class NewTestCase {
 
+    Playwright playwright;
+    Browser browser;
+
+    BrowserContext context;
+    Page page;
+
+    @BeforeClass
+    void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+    }
+
+    @AfterClass
+    void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeMethod
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterMethod
+    void closeContext() {
+        context.close();
+    }
+
     @Test
     public void testCase1() {
-        System.out.println("in test case 1 of NewTestngClass");
-        String name = "joeJumpinJolly";
-        assert name.equals("joeJumpinJolly");
+        System.out.println("in test case 1 of NewTestCase");
+        assert 1 == 1;
     }
 
     @Test
     public void testPlaywright() {
-        try (Playwright playwright = Playwright.create()) {
-            BrowserContext context;
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
-                context = browser.newContext(new Browser.NewContextOptions()
-                        .setIsMobile(true)
-                        .setHasTouch(true)
-                        .setLocale("en-US")
-                        .setGeolocation(41.889938, 12.492507)
-                        .setPermissions(List.of("geolocation")));
+        context = browser.newContext(new Browser.NewContextOptions().setIsMobile(true)
+                .setHasTouch(true).setLocale("en-US").setGeolocation(41.889938, 12.492507)
+                .setPermissions(List.of("geolocation")));
             context.tracing().start(new Tracing.StartOptions().setScreenshots(true).setSnapshots(true));
-            Page page = context.newPage();
+            page = context.newPage();
             page.navigate("https://www.openstreetmap.org/");
             Locator closePopup = page.locator("#sidebar > div.welcome.p-3 > div:nth-child(1) > div > button");
             closePopup.click();
@@ -39,7 +60,5 @@ public class NewTestCase {
             assertThat(page).hasTitle("Users' Diaries | OpenStreetMap");
             assertThat(page).hasURL("https://www.openstreetmap.org/diary");
             context.tracing().stop(new Tracing.StopOptions().setPath(Paths.get("trace.zip")));
-            // page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("colosseum-pixel2.png")));
         }
-    }
 }  
